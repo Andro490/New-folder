6c7e315a80107aababc386e9d7ebac0688f13c38
Andro490 <emelnasr@gmail.com>
2026-06-16 02:44:35 +0300

    لا

--- server.js
+++ server.js
@@ -24,7 +24,6 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
-app.options('/{*path}', cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
