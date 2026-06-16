cd7a4a7fe3c4dcbc25bf99805f722b3f6e5a5ad2
Andro490 <emelnasr@gmail.com>
2026-06-16 03:32:50 +0300

    صب

--- server.js
+++ server.js
@@ -25,7 +25,6 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
   optionsSuccessStatus: 200
 }));
-app.options('*', cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
